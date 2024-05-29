35a4faf16da377d5e673accd629cbd05aa91d062
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-05-29 21:36:34 +0600

    added to academic semester model

--- dist/app/modules/academicSemester/academicSemester.model.js
+++ dist/app/modules/academicSemester/academicSemester.model.js
@@ -1,1 +1,34 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.academicSemesterModel = void 0;
+const mongoose_1 = require("mongoose");
+const academicSemester_constant_1 = require("./academicSemester.constant");
+const acdemicSemesterSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: true,
+        enum: academicSemester_constant_1.AcademicSemesterName,
+    },
+    year: {
+        type: String,
+        required: true,
+    },
+    code: {
+        type: String,
+        required: true,
+        enum: academicSemester_constant_1.AcademicSemesterCode,
+    },
+    startMonth: {
+        type: String,
+        required: true,
+        enum: academicSemester_constant_1.Months,
+    },
+    endMonth: {
+        type: String,
+        required: true,
+        enum: academicSemester_constant_1.Months,
+    },
+}, {
+    timestamps: true,
+});
+exports.academicSemesterModel = (0, mongoose_1.model)("AcademicSemester", acdemicSemesterSchema);
