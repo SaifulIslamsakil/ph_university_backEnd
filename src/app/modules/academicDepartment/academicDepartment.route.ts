a8484186aecd90a4c1e6966eee85f454c8a2a88a
programin.saifulislam@gmail.com <programin.saifulislam@gmail.com>
2024-06-02 16:57:21 +0600

    added to Academic Department Controllers file

--- src/app/modules/academicDepartment/academicDepartment.route.ts
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -1,13 +1,14 @@
 import express from "express"
 import validateRequest from "../../midelware/validateRequest"
 import { AcademicDepartmentValidation } from "./academicDepartment.validation"
+import { AcademicDepartmentControllers } from "./academicDepartment.controller"
 
 const route = express.Router()
 
-route.post("/create-academic-department", validateRequest(AcademicDepartmentValidation.createAcademicDepartmentValidationSchema))
-route.get("/",)
-route.get("/:departmentId",)
-route.put("/:departmentId", validateRequest(AcademicDepartmentValidation.updateAcademicDepartmentValidationSchema))
+route.post("/create-academic-department", validateRequest(AcademicDepartmentValidation.createAcademicDepartmentValidationSchema), AcademicDepartmentControllers.createAcademicDepartmemt)
+route.get("/", AcademicDepartmentControllers.getAllAcademicDepartments)
+route.get("/:departmentId", AcademicDepartmentControllers.getSingleAcademicDepartment)
+route.put("/:departmentId", validateRequest(AcademicDepartmentValidation.updateAcademicDepartmentValidationSchema), AcademicDepartmentControllers.updateAcademicDeartment)
 
 
 
